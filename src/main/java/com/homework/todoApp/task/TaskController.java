519852ca16b0f1c134b5961d095c35571020d29c
ZTatman <tatmanzachariahit@gmail.com>
2021-09-09 16:03:09 -0400

    Added exception handling for missing resources

--- src/main/java/com/homework/todoApp/task/TaskController.java
+++ src/main/java/com/homework/todoApp/task/TaskController.java
@@ -3,14 +3,9 @@ package com.homework.todoApp.task;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class TaskController {
 
@@ -46,4 +41,11 @@ public class TaskController {
   public void deleteTask(@PathVariable Long id) {
     taskService.deleteTask(id);
   }
+
+//  DELETE ALL
+  @DeleteMapping("/api/tasks")
+  public void deleteAllTasks() {
+    taskService.deleteAllTasks();
+  }
+
 }
